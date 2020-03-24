66a2c05f6b0b5ab0c18e435f7a2ab185170cffa7
rageshwari <ragu.bodke@gmail.com>
2020-03-24 20:37:26 +0530

    Starting a Game by resetting the board

--- ticTacToeGame.sh
+++ ticTacToeGame.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -x
 #Welcome to Tic Tac Toe Game
+
+declare -a gameBoard
+gameBoard=(" " " " " " " " " " " " " " " " " ")
+echo "Welcome to tic tac toe game"
+
+echo "${gameBoard[0]}  | ${gameBoard[1]} | ${gameBoard[2]}"
+echo "-----------"
+echo "${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
+echo "-----------"
+echo "${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
+echo $gameBoard
+
+
+
