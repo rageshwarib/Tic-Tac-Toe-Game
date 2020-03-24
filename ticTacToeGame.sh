f01c33c13267517a275c89e8a056a296c2907499
rageshwari <ragu.bodke@gmail.com>
2020-03-24 21:59:01 +0530

    Make a toss and decide who plays first

--- ticTacToeGame.sh
+++ ticTacToeGame.sh
@@ -8,11 +8,11 @@ declare -a gameBoard
 gameBoard=(" " " " " " " " " " " " " " " " " ")
 
 #Gameboard skeleton
-echo "${gameBoard[0]}  | ${gameBoard[1]} | ${gameBoard[2]}"
-echo "-----------"
-echo "${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
-echo "-----------"
-echo "${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
+echo "         ${gameBoard[0]}  | ${gameBoard[1]} | ${gameBoard[2]}"
+echo "         -----------"
+echo "         ${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
+echo "         -----------"
+echo "         ${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
 echo $gameBoard
 
 #To check that which letter assign to palyer 
@@ -24,3 +24,11 @@ else
 	echo "You have assigned :" $X
 fi
 
+#Make a toss and decide who play first
+firstTurn=$((RANDOM%2))
+if [[ $firstTurn == 0 ]]
+then
+	echo "User plays first"
+else
+	echo "Computer plays first"
+fi
