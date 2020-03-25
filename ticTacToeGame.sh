354f9dcb57c2dc7469724838a58038df4e197cf9
rageshwari <ragu.bodke@gmail.com>
2020-03-26 00:29:49 +0530

    Player turn sees the valid cells

--- ticTacToeGame.sh
+++ ticTacToeGame.sh
@@ -2,33 +2,89 @@
 #Welcome to Tic Tac Toe Game
 X="X"
 O="O"
-
+o=0
+x=1
 echo "Welcome to tic tac toe game"
 declare -a gameBoard
 gameBoard=(" " " " " " " " " " " " " " " " " ")
-
-#Gameboard skeleton
-echo "         ${gameBoard[0]}  | ${gameBoard[1]} | ${gameBoard[2]}"
-echo "         -----------"
-echo "         ${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
-echo "         -----------"
-echo "         ${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
-echo $gameBoard
+function printGameBoard()
+{
+	#Gameboard skeleton
+	echo "         ${gameBoard[0]}  | ${gameBoard[1]} | ${gameBoard[2]}"
+	echo "         -----------"
+	echo "         ${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
+	echo "         -----------"
+	echo "         ${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
+}
 
 #To check that which letter assign to player
-letterCheck=$((RANDOM%2))
-if [[ $letterCheck == 0 ]] 
-then 
-	echo "You have assigned :" $O
-else
-	echo "You have assigned :" $X
-fi
-
+function letterCheck()
+{
+	letterCheck=$((RANDOM%2))
+		case $letterCheck in
+		$o)
+			playerLetter=$O ;;
+		$x)
+			playerLetter=$X ;;
+		esac
+	echo $playerLetter
+}
 #Make a toss and decide who play first
-firstTurn=$((RANDOM%2))
-if [[ $firstTurn == 0 ]]
-then
-	echo "User plays first"
-else
-	echo "Computer plays first"
-fi
+function getTurn()
+{
+	firstTurn=$((RANDOM%2))
+	if [[ $firstTurn == 0 ]]
+	then
+		echo "Player plays first"
+	else
+		echo "Computer plays first"
+	fi
+}
+
+#Choose a valid cells during player's turn
+function  placePiece()
+{
+	playerLetter=$1
+	echo "Enter the position you want to place your letter"
+	read place
+	if [[ $place>=0 && $place<=8 ]]
+	then
+		case $place in
+		0)
+			${gameBoard[0]}
+			$playerLetter ;;
+		1)
+			${gameBoard[1]}
+			$playerLetter ;;
+		2)
+			${gameBoard[2]}
+			$playerLetter ;;
+		3)
+			${gameBoard[3]}
+			$playerLetter ;;
+		4)
+			${gameBoard[4]}
+			$playerLetter ;;
+		5)
+			${gameBoard[5]}
+			$playerLetter ;;
+		6)
+			${gameBoard[6]}
+			$playerLetter ;;
+		7)
+			${gameBoard[7]}
+			$playerLetter ;;
+		8)
+			${gameBoard[8]}
+			$playerLetter ;;
+		*)
+			echo "Invalid Placement"
+		esac
+	fi
+	printGameBoard
+}
+
+letter="$( letterCheck)"
+getTurn
+placePiece $letter
+
