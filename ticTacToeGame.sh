036cb3fe182e4c85b7e31e3ae3a5dfd3330812f3
rageshwari <ragu.bodke@gmail.com>
2020-03-26 11:18:41 +0530

    Checking winning condition and also check win or draw

--- ticTacToeGame.sh
+++ ticTacToeGame.sh
@@ -1,90 +1,111 @@
 #!/bin/bash -x
-#Welcome to Tic Tac Toe Game
+
 X="X"
 O="O"
-o=0
-x=1
-echo "Welcome to tic tac toe game"
+DOT=0
+CROSS=1
+USER=0
+COMPUTER=1
 declare -a gameBoard
 gameBoard=(" " " " " " " " " " " " " " " " " ")
-function printGameBoard()
+function displayBoard()
 {
-	#Gameboard skeleton
-	echo "         ${gameBoard[0]}  | ${gameBoard[1]} | ${gameBoard[2]}"
-	echo "         -----------"
-	echo "         ${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
-	echo "         -----------"
-	echo "         ${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
+	echo "Welcome to tic tac toe game"
+	echo "		${gameBoard[0]}  | ${gameBoard[1]} | ${gameBoard[2]}"
+	echo "		-----------"
+	echo "		${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
+	echo "		-----------"
+	echo "		${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
 }
-
-#To check that which letter assign to player
-function letterCheck()
+function playerTurn()
+{
+	playerLetter=$1
+	printf "Enter any number between 0 to 8 : "
+	read response
+	if [[ $response>=0 && $response<=8 ]]
+	then
+		if [[ "${gameBoard[$response]}"!=X || "${gameBoard[$response]}"!=O ]]
+		then
+			gameBoard[$response]="$playerLetter"
+		else
+			playerTurn $playerLetter
+		fi
+	else
+		printf "Invalid input\n"
+	fi
+	displayBoard
+}
+function checkWin()
+{
+	letter=$1
+	if [ "${gameBoard[0]}${gameBoard[1]}${gameBoard[2]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameBoard[3]}${gameBoard[4]}${gameBoard[5]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameBoard[6]}${gameBoard[7]}${gameBoard[8]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameBoard[0]}${gameBoard[3]}${gameBoard[6]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameBoard[1]}${gameBoard[4]}${gameBoard[7]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameBoard[2]}${gameBoard[5]}${gameBoard[8]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameBoard[0]}${gameBoard[4]}${gameBoard[8]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameBoard[2]}${gameBoard[4]}${gameBoard[6]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	else
+		flag=0
+		for((index=0;index<${#gameBoard[@]};index++))
+		do
+			if [[ "${gameBoard[$response]}"!=X || "${gameBoard[$response]}"!=O ]]
+			then
+				flag=1
+			fi
+		done
+		if [ $flag==0 ]
+		then
+			result="draw"
+		else
+			result="change"
+		fi
+	fi
+	echo $result
+}
+function assignLetter()
 {
 	letterCheck=$((RANDOM%2))
-		case $letterCheck in
-		$o)
-			playerLetter=$O ;;
-		$x)
-			playerLetter=$X ;;
-		esac
+	case $letterCheck in
+		$DOT)
+			playerLetter=$O
+		;;
+		$CROSS)
+			playerLetter=$X
+		;;
+	esac
 	echo $playerLetter
 }
-#Make a toss and decide who play first
 function getTurn()
 {
 	firstTurn=$((RANDOM%2))
-	if [[ $firstTurn == 0 ]]
-	then
-		echo "Player plays first"
-	else
-		echo "Computer plays first"
-	fi
+	case $firstTurn in
+		$USER)
+			 echo "User plays first"
+		;;
+		$COMPUTER)
+			 echo "Computer plays first"
+		;;
+	esac
 }
-
-#Choose a valid cells during player's turn
-function  placePiece()
-{
-	playerLetter=$1
-	echo "Enter the position you want to place your letter"
-	read place
-	if [[ $place>=0 && $place<=8 ]]
-	then
-		case $place in
-		0)
-			${gameBoard[0]}
-			$playerLetter ;;
-		1)
-			${gameBoard[1]}
-			$playerLetter ;;
-		2)
-			${gameBoard[2]}
-			$playerLetter ;;
-		3)
-			${gameBoard[3]}
-			$playerLetter ;;
-		4)
-			${gameBoard[4]}
-			$playerLetter ;;
-		5)
-			${gameBoard[5]}
-			$playerLetter ;;
-		6)
-			${gameBoard[6]}
-			$playerLetter ;;
-		7)
-			${gameBoard[7]}
-			$playerLetter ;;
-		8)
-			${gameBoard[8]}
-			$playerLetter ;;
-		*)
-			echo "Invalid Placement"
-		esac
-	fi
-	printGameBoard
-}
-
-letter="$( letterCheck)"
+letter="$( assignLetter)"
 getTurn
-placePiece $letter
-
+playerTurn $letter
+checkWin $playerLetter
